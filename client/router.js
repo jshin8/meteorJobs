5d7c0d8d21b018060c62b18502535063eef0c47e
jshin8 <joshshin8@gmail.com>
2016-01-22 02:17:47 -0800

    adds log-in, roles, rouroutes

---
+++ client/router.js
@@ -0,0 +1,62 @@
+//home route
+FlowRouter.route('/', {
+  name: 'home',
+  action: function () {
+    BlazeLayout.render('layout', {content: 'home'});
+  }
+});
+
+//admin routes
+FlowRouter.route('/admin', {
+  action: function () {
+    BlazeLayout.render('layout', {content: 'accountsAdmin'});
+  }
+});
+
+//user routes
+FlowRouter.route('/client/', {
+  action: function () {
+    BlazeLayout.render('layout', {content: 'jobsListClient'});
+  }
+});
+
+
+
+//redirects to '/' when logged out
+ Tracker.autorun(function () {
+  if (!(Meteor.userId())){
+  	FlowRouter.go('home');
+  }
+});
+
+
+
+
+
+
+
+// Router.configure({
+// 	layoutTemplate: 'layout'
+// });
+
+// Router.map(function() {
+// 	this.route('home', {
+// 		path: '/',
+// 		template: 'home'
+// 	});
+
+// 	this.route('admin', {
+// 		path:'/admin',
+// 		template: 'accountsAdmin',
+// 		onBeforeAction: function() {
+// 			if (Meteor.loggingIn()) {
+// 				this.render(this.loadingTemplate);
+// 			} else if(!Roles.userIsInRole(Meteor.user(), ['admin'])) {
+// 				console.log('redirecting');
+// 				this.redirect('/');
+// 			} else {
+// 				this.next();
+// 			}
+// 		}
+// 	});
+// });
